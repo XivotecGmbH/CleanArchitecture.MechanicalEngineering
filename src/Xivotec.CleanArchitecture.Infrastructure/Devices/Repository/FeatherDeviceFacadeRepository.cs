bd09da3981cb20b203aaf96a4e35c44e7bd21e15
Xivotec GmbH <148527540+XivotecGmbH@users.noreply.github.com>
2024-05-14 14:51:37 +0200

    2.0 release

--- src/Xivotec.CleanArchitecture.Infrastructure/Devices/Repository/FeatherDeviceFacadeRepository.cs
+++
@@ -1,6 +0,0 @@
-﻿using Xivotec.CleanArchitecture.Infrastructure.Devices.Facade;
-using Xivotec.CleanArchitecture.Infrastructure.Persistence.Common;
-
-namespace Xivotec.CleanArchitecture.Infrastructure.Devices.Repository;
-
-public class FeatherDeviceFacadeRepository : RuntimeRepository<FeatherDeviceFacade>;
--- src/Xivotec.CleanArchitecture.Infrastructure/Devices/Repository/FeatherDeviceRepository.cs
+++
@@ -1,8 +0,0 @@
-﻿using Xivotec.CleanArchitecture.Domain.FeatherDeviceAggregate.Entities;
-using Xivotec.CleanArchitecture.Infrastructure.Persistence.Common;
-
-namespace Xivotec.CleanArchitecture.Infrastructure.Devices.Repository;
-
-public class FeatherDeviceRepository : RuntimeRepository<FeatherDevice>
-{
-}
